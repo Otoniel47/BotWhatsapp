2bbca7195e88df95f9c8a3db27df7285cb5760a6
Otoniel47 <otoniel.candido47@gmail.com>
2021-12-12 20:09:14 -0300

    Projeto Bot para whatsapp C# e Selenium

--- BotWhatsappCurso/Program.cs
+++ BotWhatsappCurso/Program.cs
@@ -18,43 +18,84 @@ namespace BotWhatsappCurso
 
             try
             {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Carrega a página");
                 driver.LoadPage("https://web.whatsapp.com/");
                 Thread.Sleep(TimeSpan.FromSeconds(10));
 
                 //Lista de links de grupo
+                Console.WriteLine("Listando Grupos");
                 List<string> listaGrupo = new List<string>();
-                listaGrupo.Add("Link do grupo");
-                listaGrupo.Add("Link do grupo");
+                listaGrupo.Add("https://chat.whatsapp.com/FvXJii3WiSU2a5DSLmLOSC");
+                listaGrupo.Add("https://chat.whatsapp.com/CTceLLKtdXS7XGl4TtbFPw");
 
                 for (int x = 0; x < listaGrupo.Count; x++)
                 {
                     //Entrando Grupo
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                    Console.WriteLine("Entrando em um grupo listado: " + x);
+                    Thread.Sleep(TimeSpan.FromSeconds(3));
                     driver.LoadPage(listaGrupo[x]);
 
+                    Console.WriteLine("Cliando Entrar");
                     var btnEntrarGrupo = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//*[@id='action-button']")));
                     btnEntrarGrupo.Click();
 
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                    Console.WriteLine("Confirmando Entrada");
+
+                    for (int i = 5; i > 0; i--)
+                    {
+                        Console.WriteLine("Aguardando: " + i + " segundos");
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+                    }
+
                     var linkEntrarGrupo = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//*[@id='fallback_block']/div/div/a")));
                     linkEntrarGrupo.Click();
 
-                    Thread.Sleep(TimeSpan.FromSeconds(3));
+                    Console.WriteLine("Entrando no grupo");
+                    for (int i = 5; i > 0; i--)
+                    {
+                        Console.WriteLine("Aguardando: " + i + " segundos");
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+                    }
+
                     var confirmarEntrada = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//*[@id='app']/div[1]/span[2]/div[1]/div/div/div/div/div/div[2]/div[2]")));
                     confirmarEntrada.Click();
 
                     //Escrevendo Grupo
-                    Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                    for (int i = 5; i > 0; i--)
+                    {
+                        Console.WriteLine("Aguardando: " + i + " segundos");
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+                    }
+
+                    Console.WriteLine("Escrevendo no grupo");
                     var campoDeTexto = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//*[@id='main']/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]")));
                     campoDeTexto.SendKeys("Aula do curso desenvolvimento de bots com C# e Selenium");
+
+                    for (int i = 5; i > 0; i--)
+                    {
+                        Console.WriteLine("Aguardando: " + i + " segundos");
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+                    }
+
                     campoDeTexto.SendKeys(Keys.Enter);
 
+                    for (int i = 5; i > 0; i--)
+                    {
+                        Console.WriteLine("Aguardando: " + i + " segundos");
+                        Thread.Sleep(TimeSpan.FromSeconds(1));
+                    }
+
+                    Console.WriteLine("Clicando menu");
                     var menu = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//*[@id='main']/header/div[3]/div/div[2]/div/div/span")));
                     menu.Click();
 
+                    Console.WriteLine("Clicando botão sair");
                     var btnSair = driver.FindElement(By.XPath("//*[@id='app']/div[1]/span[4]/div/ul/div/div/li[5]/div[1]"));
                     btnSair.Click();
 
+                    Console.WriteLine("Confirmando saida do grupo: " + x);
                     var confirmarSaida = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//*[@id='app']/div[1]/span[2]/div[1]/div/div/div/div/div/div[2]/div[2]/div/div")));
                     confirmarSaida.Click();
                 }
